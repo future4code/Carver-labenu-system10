6663bd463eae8dc4a4580d98d4071ca73842627a
Luiza Cioffi <luizagac@gmail.com>
2022-02-17 20:38:09 -0300

    Endpoints de Turma

--- src/endpoints/turma/criarTurma.ts
+++ src/endpoints/turma/criarTurma.ts
@@ -1,5 +1,27 @@
 import { Request, Response } from "express";
+import { connection } from "../../Data/connection";
+import { TipoTurma } from "../../types/TipoTurma";
 
-export const criarTurmas = async (req: Request, res: Response) => {
+export const criarTurma = async (req: Request, res: Response) => {
+  try {
+    const {nome, docentes, estudantes, modulo} =req.body;
 
+    if (!nome || !docentes || !estudantes || !modulo){
+      throw new Error("Campos necessários não foram informados");
+    }
+
+    const turma:TipoTurma = {
+      id:Date.now().toString(),
+      nome,
+      docentes,
+      estudantes,
+      modulo,
+    }
+
+    await connection("labesystem_turmas").insert(turma);
+    res.status(200).send("Turma criada!")
+
+  } catch (error: any) {
+      res.status(400).send({message: error.message})
+  }
 }
---
+++ src/types/TipoTurma.ts
@@ -0,0 +1,7 @@
+export type TipoTurma = {
+    id: string
+    nome: string
+    docentes: string
+    estudantes: string
+    modulo: number
+  }
